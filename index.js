a53f045b5da7204f525084f978209198dedbd6f9
wujia <wujia_84123@126.com>
2017-06-03 09:46:48 +0000

    update

--- index.js
+++ index.js
@@ -1,4 +1,15 @@
-e strict'
+//服务账号
+//productKey:1000193944
+//productSecret:pP0ySZAr2JmbhSl5
+//
+//第一台设备账号
+//deviceName:zhuxiaozhi_001
+//deviceId:0gZtZqdZnl9FONHSL
+//deviceSecret:GTlw97YqiF3U7jhj
+
+
+'use strict'
+var gpio = require("gpio");
 
 const mqtt = require('mqtt');
 // 加解密
@@ -21,8 +32,7 @@ var make_client_id = (productKey, deviceName, deviceType, sdkVersion) => {
 }
 
 //生成UserName
-var make_username = (productKey, productSecret, deviceName, deviceSecret) =>
-{
+var make_username = (productKey, productSecret, deviceName, deviceSecret) => {
     var key = `${productKey}${productSecret}${deviceName}${deviceSecret}`;
     const hash = crypto.createHash('md5');
     hash.update(key);
@@ -45,21 +55,47 @@ var settings = {
 var client = mqtt.connect(settings);
 
 var doorState = "closed";
+var ready = false
+// 核心代码: 这里开始调用GPIO 接口 做硬件控制 使用第21 针脚   右下角
+var doorIO = gpio.export(21, {
+    // 数据输出端口
+    direction: 'out',
+    // ready 是一步的
+    ready: function () { 
+        ready = true;
+    }
+});
 
 client.on('message', function (topic, message) {
     // message is Buffer
-    console.log(message.toString())
+       console.log(topic+message.toString())
+
+    if (ready) {
+        doorIO.set(function () {
+            setTimeout(function () {
+                doorIO.set(0)
+                console.log('set0');
+            }, 7000)
+
+            console.log(doorIO.value);    // should log 1
+        });
+    }
+
+
 })
 
 client.on('connect', function () {
     console.log('>>> connected')
+    // 订阅 开门消息
+    //https://www.npmjs.com/package/mqtt#subscribe   # 是通配符  或者直接使用  /1000193944/zhuxiaozhi_001/openDoor	,可以是数组或字符串
+    //  client.subscribe(['#/get','#/oepnDoor'])
+    client.subscribe('/1000193944/zhuxiaozhi_001/openDoor')
     setInterval(
         ()=> {
             doorState = doorState == "closed" ? "open" : "closed";
             client.publish(topic, doorState);
         },
-        3000
+        10000
     );
 
 })
-
---
+++ test.js
@@ -0,0 +1,36 @@
+ 
+'use strict'
+var gpio = require("gpio");
+
+var open =false;
+
+var button = gpio.export(21, {
+    
+    direction: 'in',
+     
+    ready: function () { 
+        setInterval(function(){
+deng.set(button.value)
+console.log(button.value);
+         // if(button.value)
+	//{
+	
+	  //open = !open;
+          //;
+	//}
+      },200)
+    }
+});
+
+
+
+var deng = gpio.export(20, {
+    // 数据输出端口
+    direction: 'out',
+    // ready 是一步的
+    ready: function () { 
+        
+	deng.set(0); 
+    }
+});
+
